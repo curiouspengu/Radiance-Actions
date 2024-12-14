f21ee9a866ec2a92e72787d72326b79043ee63ab
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-12-14 11:18:59 +1100

    Create click_menu_button.py

---
+++ click_menu_button.py
@@ -0,0 +1,10 @@
+kc.tap('\\')
+for i in range(4):
+    sleep(0.1)
+    kc.tap(Key.left) 
+for i in range(5 - button_num):
+    sleep(0.1)
+    kc.tap(Key.up)
+sleep(0.1)
+kc.tap(Key.enter)
+kc.tap('\\')
